9ec55cd42b900d7fd2faa54470cfd18e9abe7df2
Patricia Booth <44758340+alpacawool@users.noreply.github.com>
2021-10-18 21:42:58 -0700

    Replay styling

--- frontend/src/components/ReplayScreen/ReplayView.js
+++ frontend/src/components/ReplayScreen/ReplayView.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import './ReplayView.css';
 
 const ReplayView = (props) => {
 
     return(
-        <div>
+        <div id="replay-div">
             <h1>Cat Breed Quiz</h1>
             <p>Congratulations! You have completed the quiz.</p>
-            <p>Final Score:</p>
-            <p><b>{props.score}</b></p>
-            <button onClick={props.clickHandler}>Replay</button>
+            <p><b>Final Score:</b></p>
+            <p id="score-text">{props.score}</p>
+            <div id="replay-button" onClick={props.clickHandler}>
+                <div id="replay-text">Replay</div>
+                <div id="replay-image"></div>
+            </div>
+
         </div>
     );
 };
